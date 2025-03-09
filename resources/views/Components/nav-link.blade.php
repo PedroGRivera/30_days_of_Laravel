fd754cf899e8a5ae9a021ee04459eaf1847640f7
PedroGRivera <PedroGRivera97@gmail.com>
2025-03-09 10:40:30 -0500

    day 5 homework completed.

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,4 +1,6 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => ''])
+
+@if($type != 'button') 
 <a 
     class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" 
     aria-current="{{ $active ? 'page' : 'false' }}"
@@ -6,3 +8,14 @@
 >
     {{ $slot }}
 </a>
+
+@else
+<a {{ $attributes }}>
+    <button
+        class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" 
+        aria-current="{{ $active ? 'page' : 'false' }}"
+    >
+        {{ $slot }}
+    </button>
+</a>
+@endif
